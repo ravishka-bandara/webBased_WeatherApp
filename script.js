348b2eb8b1d6e449431a274511012d6479d6a755
ravishka-bandara <ravishkabandara01@gmail.com>
2025-12-11 20:12:36 +0530

    feat: DOM elemnts in script.js

--- script.js
+++ script.js
@@ -7,3 +7,21 @@ const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
 
 const cityInput = document.getElementById('city-input');
 const searchBtn = document.getElementById('search-btn');
+const locationBtn = document.getElementById('location-btn');
+const cityName = document.getElementById('city-name');
+const temperature = document.getElementById('temperature');
+const weatherIcon = document.getElementById('weather-icon');
+const weatherDescription = document.getElementById('weather-description');
+const humidity = document.getElementById('humidity');
+const windSpeed = document.getElementById('wind-speed');
+const feelsLike = document.getElementById('feels-like');
+const sunTimes = document.getElementById('sun-times');
+const currentDate = document.getElementById('current-date');
+const errorMessage = document.getElementById('error-message');
+const errorText = document.getElementById('error-text');
+const loading = document.getElementById('loading');
+const unitButtons = document.querySelectorAll('.unit-btn');
+
+
+//global varieables
+
