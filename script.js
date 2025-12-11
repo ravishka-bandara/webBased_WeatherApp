0b61553304b7a6220478d9bb7a83b9f12025843b
ravishka-bandara <ravishkabandara01@gmail.com>
2025-12-11 19:08:56 +0530

    feat: dom elements in script.js

--- script.js
+++ script.js
@@ -3,3 +3,7 @@
 const API_KEY = '';
 const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
 
+//DOM elements
+
+const cityInput = document.getElementById('city-input');
+const searchBtn = document.getElementById('search-btn');
